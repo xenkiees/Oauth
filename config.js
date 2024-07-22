cff95e76b63b70439f90b5ca9ef5c2da987abdc6
xenkiees <jayden2known@gmail.com>
2024-07-22 16:18:09 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token: "OTk3NDk3MDM5MDIzNzcxNjk5.Grx5su.orAjygFJuBPH7I2WLtcEN5Ppk2Jh1kdyVk8tlE",
+    token: "MTI1NjMyOTAyNTUwOTM5MjQwNQ.GW0yeE.Ge7UqNKFSgZcwVDiMVfybKIJSBrErcxLA6p744",
     prefix: ";",
-    owners: [""],
-    authLink: "https://discord.com/api/oauth2/authorize?client_id=997497039023771699&redirect_uri=https%3A%2F%2F.repl.co&response_type=code&scope=identify%20guilds.join",
+    owners: ["927301113382846464"],
+    authLink: "https://discord.com/oauth2/authorize?client_id=1256329025509392405&response_type=code&redirect_uri=https%3A%2F%2Ff6d5199a-4ba3-425e-bf06-040c429ba8d9-00-1jjv0q8lznc7o.picard.replit.dev%2F&scope=identify+guilds.join",
 
 }
